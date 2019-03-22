daf186b8839d6b3f5a4c3a0d4142a622fb5ef979
Ali Khudiyev <xudiyev-ali@mail.ru>
2019-03-22 08:21:41 +0400

    ready to run

--- src/detect.c
+++ src/detect.c
@@ -25,53 +25,67 @@ int main(int argc, char* argv[]){
     // if(get_error(argc, argv)) return 1;
 
     unsigned n_proc=0, exit_code=0, interval=0;
-    char time_format[15], cmd[50], args[100];
+    char time_format[30], *cmd, *args;
     int tube[2];
     Vector *old_buff=NULL, *new_buff=NULL;
     // old_buff=new_Vector(100, old_buff);
     new_buff=new_Vector(100, new_buff);
 
-    void* mem=malloc(1);
-    char* old_buf=(char*)mmap(NULL, 100*sizeof(char), PROT_READ | PROT_WRITE, MAP_ANONYMOUS | MAP_SHARED, 0, 0);
+    char* old_buf=(char*)mmap(NULL, 10000*sizeof(char), PROT_READ | PROT_WRITE, MAP_ANONYMOUS | MAP_SHARED, 0, 0);
     CHECK_MEMORY(old_buf);
-    memcpy(old_buf, "_", 1);
+    cmd=(char*)mmap(NULL, 100*sizeof(char), PROT_READ | PROT_WRITE, MAP_ANONYMOUS | MAP_SHARED, 0, 0);
+    args=(char*)mmap(NULL, 100*sizeof(char), PROT_READ | PROT_WRITE, MAP_ANONYMOUS | MAP_SHARED, 0, 0);
+    old_buf[0]='\0';
+    time_format[0]='\0';
 
-    int opt;
+    int opt, cnt=0;
     while((opt=getopt(argc, argv, "cl:t:i:"))!=-1){
         // printf("\tparsing..\n");
+        ++cnt;
         switch(opt){
             case 'c':
-                exit_code=1;
-                printf("Exit code status: %d\n", exit_code);
+                if(!exit_code){
+                    exit_code=1;
+                    // printf("Exit code status: %d\n", exit_code);
+                }
                 break;
             case 'l':
-                n_proc=atoi(optarg);
-                printf("Limit: %d\n", n_proc);
+                if(!n_proc){
+                    n_proc=atoi(optarg);
+                    // printf("Limit: %d\n", n_proc);
+                }
                 break;
             case 't':
-                strcpy(time_format, optarg);
-                printf("Time format: %s\n", time_format);
+                if(time_format[0]=='\0'){
+                    strcpy(time_format, optarg);
+                    // printf("Time format: %s\n", time_format);
+                }
                 break;
             case 'i':
-                interval=atoi(optarg);
-                printf("Interval: %d\n", interval);
+                if(!interval){
+                    interval=atoi(optarg);
+                    // printf("Interval: %d\n", interval);
+                }
                 break;
             default:
-                printf("Other argument: %s\n", optarg);
+                // printf("Other argument: %s\n", optarg);
                 break;
         }
+        if(n_proc && interval && time_format[0]!='\0') break;
     }
-    // printf("optind: %d\n", optind);
+    // printf("optind: %d\n\n", optind);
+    args[0]='\0';
+    cmd[0]='\0';
     for(int i=optind;i<argc;++i){
         if(i==optind){
             strcpy(cmd, argv[i]);
         } else{
-            strcat(strcat(args, argv[i]), " ");
+            strcat(strcat(args, argv[i]), ",");
         }
     }
-    printf("Command: %s\n", cmd);
-    printf("Arguments: %s\n", args);
-    if(!n_proc || !interval || !strlen(cmd) || !strlen(args) || !strlen(time_format)){
+    // printf("Command: %s\n", cmd);
+    // printf("Arguments: %s\n", args);
+    if(!n_proc || !interval || cmd[0]=='\0' || time_format[0]=='\0'){
         printf("Not enough information has been given!\n");
         exit(1);
     }
@@ -87,27 +101,41 @@ int main(int argc, char* argv[]){
         pid_t pid=fork();
         if(pid==-1){ EXCEPT(PIPE, "Couldn't create a process!"); }
         else if(pid==0){
-            printf("\tC\n");
             pid_t new_pid=fork();
             if(new_pid==-1){ EXCEPT(PIPE, "Couldn't create a process!"); }
             else if(new_pid==0){
-                printf("\t\tCC\n");
                 close(tube[0]);
                 if(dup2(tube[1], 1)==-1){ EXCEPT(NFILE, "Duplication failed!"); }
 
-                char* args[3]={"ls", NULL};
-                if(execvp(args[0], args)<0){ EXCEPT(FATAL, "Execution failed!\n"); }
+                char* args_[50]={"ls", "obj/", NULL};
+                args_[0]=cmd;
+                unsigned beg, in=0, j=1;
+                char tmp[100][100];
+                for(unsigned t=0;t<100;++t) tmp[t][0]='\0';
+                for(;j<50;++j){
+                    if(in>=strlen(args)) break;
+                    for(beg=in;args[in]!=',';++in) tmp[j-1][in-beg]=args[in];
+                    // tmp[j-1][in]='\0';
+                    args_[j]=tmp[j-1];
+                    ++in;
+                }
+                args_[j]=NULL;
+                // printf("Executing: %s %s %s %s\n", args_[0], args_[1], args_[2], args_[3]);
+                if(execvp(args_[0], args_)<0){ EXCEPT(FATAL, "Execution failed!\n"); }
             }
             else{
-                wait(NULL);
-                printf("\t\tPP\n");
+                int stat, exit_stat;
+                wait(&stat);
+                if((exit_stat=WEXITSTATUS(stat))){
+                    exit(exit_stat);
+                }
                 close(tube[1]);
-                char buff[100];
-                read(tube[0], buff, 100);
+                char buff[10000];
+                read(tube[0], buff, 10000);
                 close(tube[0]);
-                if(memcmp(buff, old_buf, 100) || old_buf[0]=='_'){
-                    printf("Context: %s\n", buff);
-                    memcpy(old_buf, buff, 100);
+                if(memcmp(buff, old_buf, 10000)){
+                    printf("%s", buff);
+                    memcpy(old_buf, buff, 10000);
                 }
                 exit(0);
             }
@@ -115,8 +143,9 @@ int main(int argc, char* argv[]){
         else{
             int stat;
             wait(&stat);
-            printf("\tP\n");
-            if(exit_code) printf("Exit %u\n", WEXITSTATUS(stat));
+            int exit_stat;
+            if(exit_code) printf("exit %u\n\n", (exit_stat=WEXITSTATUS(stat)));
+            if(exit_stat) return 1;
             if(i<n_proc-1){
             /*if(*/sleep(interval/1000);//==-1){ EXCEPT(WARNING, "The program didn't consider the given interval!"); }
                 time(&raw_time);
